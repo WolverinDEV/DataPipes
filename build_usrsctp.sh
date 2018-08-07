f8b8024caacbef279cd8951a14afe8fd464cc32a
WolverinDEV <git@teaspeak.de>
2018-08-07 19:42:48 +0000

    Added option to disable tests

---
+++ build_usrsctp.sh
@@ -0,0 +1,16 @@
+cd libraries/usrsctp/
+if [ -d build ]; then
+	rm -r build
+fi
+mkdir build && cd build
+cmake .. -DCMAKE_C_FLAGS="-fPIC"
+if [ $? -ne 0 ]; then
+	echo "failed to generate cmake project!"
+	exit 1
+fi
+make -j 12
+if [ $? -ne 0 ]; then
+	echo "failed to build!"
+	exit 1
+fi
+sudo make install
