fbfb652bccad02abcceae90306f368fb79d493d6
WolverinDEV <git@mcgalaxy.de>
2019-06-13 18:36:48 +0200

    Starting some video experiments

--- test/video/ivfenc.cpp
+++ test/video/ivfenc.cpp
@@ -1,5 +1,56 @@
-//
-// Created by wolverindev on 12.06.19.
-//
 
-#include "ivfenc.h"
+/*
+ *  Copyright (c) 2013 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#include <zconf.h>
+#include "./ivfenc.h"
+#include "vpx/vpx_encoder.h"
+
+inline void mem_put_le16(char* buffer, int value) {
+	buffer[0] = (value >> 0) & 0xFF;
+	buffer[1] = (value >> 8) & 0xFF;
+}
+
+inline void mem_put_le32(char* buffer, int value) {
+	buffer[0] = (value >>  0) & 0xFF;
+	buffer[1] = (value >>  8) & 0xFF;
+	buffer[2] = (value >> 16) & 0xFF;
+	buffer[3] = (value >> 24) & 0xFF;
+}
+
+void ivf_write_file_header(ivf_enc_target_t out, const struct vpx_codec_enc_cfg *cfg, unsigned int fourcc, int frame_cnt) {
+	char header[32];
+	header[0] = 'D';
+	header[1] = 'K';
+	header[2] = 'I';
+	header[3] = 'F';
+	mem_put_le16(header + 4, 0);                     // version
+	mem_put_le16(header + 6, 32);                    // header size
+	mem_put_le32(header + 8, fourcc);                // fourcc
+	mem_put_le16(header + 12, cfg->g_w);             // width
+	mem_put_le16(header + 14, cfg->g_h);             // height
+	mem_put_le32(header + 16, cfg->g_timebase.den);  // rate
+	mem_put_le32(header + 20, cfg->g_timebase.num);  // scale
+	mem_put_le32(header + 24, frame_cnt);            // length
+	mem_put_le32(header + 28, 0);                    // unused
+
+	write(out, header, 32);
+}
+void ivf_write_frame_header(ivf_enc_target_t out, int64_t pts, size_t frame_size) {
+	char header[12];
+	mem_put_le32(header, (int)frame_size);
+	mem_put_le32(header + 4, (int)(pts & 0xFFFFFFFF));
+	mem_put_le32(header + 8, (int)(pts >> 32));
+	write(out, header, 12);
+}
+void ivf_write_frame_size(ivf_enc_target_t out, size_t frame_size) {
+	char header[4];
+	mem_put_le32(header, (int)frame_size);
+	write(out, header, 4);
+}
--- test/video/ivfenc.h
+++ test/video/ivfenc.h
@@ -1,14 +1,23 @@
-//
-// Created by wolverindev on 12.06.19.
-//
+/*
+ *  Copyright (c) 2013 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
 
-#ifndef PLUGIN_IVFENC_H
-#define PLUGIN_IVFENC_H
+#pragma once
 
+#include <cstdint>
+#include <cstddef>
 
-class ivfenc {
+typedef int ivf_enc_target_t; /* file descriptor */
 
-};
+struct vpx_codec_enc_cfg;
+struct vpx_codec_cx_pkt;
 
-
-#endif //PLUGIN_IVFENC_H
+void ivf_write_file_header(ivf_enc_target_t out, const struct vpx_codec_enc_cfg *cfg, uint32_t fourcc, int frame_cnt);
+void ivf_write_frame_header(ivf_enc_target_t out, int64_t pts, size_t frame_size);
+void ivf_write_frame_size(ivf_enc_target_t out, size_t frame_size);
