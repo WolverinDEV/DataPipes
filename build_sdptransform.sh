315f9f0b8cf4119abedc338cd73e3154143b6d2e
WolverinDEV <git@mcgalaxy.de>
2019-07-01 21:49:02 +0200

    Updated build script

--- build_sdptransform.sh
+++ build_sdptransform.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 cd libraries/sdptransform/
-if [ -d build ]; then
+if [[ -d build ]]; then
 	sudo rm -r build
 fi
 mkdir build && cd build
-cmake .. -DCMAKE_CXX_FLAGS="-fPIC -Wno-error=format-truncation=" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
-if [ $? -ne 0 ]; then
+cmake .. -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
+if [[ $? -ne 0 ]]; then
 	echo "failed to generate cmake project!"
 	exit 1
 fi
 make -j 12
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
 	echo "failed to build!"
 	exit 1
 fi
--- build_usrsctp.sh
+++ build_usrsctp.sh
@@ -1,15 +1,16 @@
+#!/usr/bin/env bash
 cd libraries/usrsctp/
-if [ -d build ]; then
+if [[ -d build ]]; then
 	sudo rm -r build
 fi
 mkdir build && cd build
-cmake .. -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
-if [ $? -ne 0 ]; then
+cmake .. -DCMAKE_C_FLAGS="-fPIC  -Wno-error=format-truncation=" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
+if [[ $? -ne 0 ]]; then
 	echo "failed to generate cmake project!"
 	exit 1
 fi
 make -j 12
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
 	echo "failed to build!"
 	exit 1
 fi
