cd92046ec1a91fa6b904427afeda840fab4cc338
WolverinDEV <git@mcgalaxy.de>
2019-07-02 13:20:17 +0200

    Fixed usrsctp warnings

--- build_usrsctp.sh
+++ build_usrsctp.sh
@@ -4,7 +4,7 @@ if [[ -d build ]]; then
 	sudo rm -r build
 fi
 mkdir build && cd build
-cmake .. -DCMAKE_C_FLAGS="-fPIC  -Wno-error=format-truncation=" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
+cmake .. -DCMAKE_C_FLAGS="-fPIC  -Wno-error=format-truncation= -Wno-error=address-of-packed-member" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
 if [[ $? -ne 0 ]]; then
 	echo "failed to generate cmake project!"
 	exit 1
