fcce8b6a0f9b81b8f3692013ecd5a74d4690dba1
WolverinDEV <git@did.science>
2021-02-07 19:15:28 +0100

    Fixed compile error when compiling without openssl

--- cmake/sources.cmake
+++ cmake/sources.cmake
@@ -1,16 +1,21 @@
 # Basic library sources
 list(APPEND SOURCE_FILES
-        src/ws.cpp
-        src/tls.cpp
         src/pipeline.cpp
         src/buffer.cpp
         src/allocator.cpp
 
-        src/ssl/ssl.cpp
-        src/ssl/ssl_bio.cpp
-
         src/http/http.cpp
 )
+
+if (NOT CRYPTO_TYPE STREQUAL "none")
+    list(APPEND SOURCE_FILES
+            src/ws.cpp
+            src/tls.cpp
+
+            src/ssl/ssl.cpp
+            src/ssl/ssl_bio.cpp
+    )
+endif ()
 
 if (NOT WIN32)
     message("We're not on windows")
